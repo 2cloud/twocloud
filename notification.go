4189ad3ee5e270574cd4a6fd01833aec58e232c7
Paddy Foran <foran.paddy@gmail.com>
2012-11-25 15:12:11 -0500

    Stubbed out notifications.

--- notification.go
+++ notification.go
@@ -1,4 +1,28 @@
 package twocloud
 
+import (
+	"secondbit.org/ruid"
+	"time"
+)
+
 type Notification struct {
+	ID       ruid.RUID `json:"id,omitempty"`
+	Nature   string    `json:"nature,omitempty"`
+	Body     string    `json:"body,omitempty"`
+	Unread   bool      `json:"unread,omitempty"`
+	ReadBy   ruid.RUID `json:"read_by,omitempty"`
+	TimeRead time.Time `json:"time_read,omitempty"`
+	Sent     time.Time `json:"sent,omitempty"`
+}
+
+func (r *RequestBundle) GetNotificationsByDevice(device Device, before, after ruid.RUID, count int) ([]Notification, error) {
+	return []Notification{}, nil
+}
+
+func (r *RequestBundle) GetNotificationsByUser(user User, before, after ruid.RUID, count int) ([]Notification, error) {
+	return []Notification{}, nil
+}
+
+func (r *RequestBundle) GetNotification(id ruid.RUID) (Notification, error) {
+	return Notification{}, nil
 }
